98d629093660a5751a8de48fbb7ffcaee8577d36
Checker8763 <tomkocha@gmail.com>
2022-01-02 22:04:47 +0100

    Added debug toast to coreAttack Trigger

--- scripts/main.js
+++ scripts/main.js
@@ -10,5 +10,9 @@ function cachedLoadSound(path) {
 Events.on(Trigger.teamCoreDamage, (_) => {
   const mySound = cachedLoadSound("base_under_attack");
   // engine will spawn this sound at this location (X,Y)
+  // TODO: get player position
   mySound.at(1, 1);
+  //Debug toast to add something visually
+  // cause the sound maybe not in close proximity to player
+  Vars.ui.hudfrag.showToast("Core Attacked ?!?!??!?!?");
 });
